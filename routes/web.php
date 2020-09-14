ae0e8662bc059f30a7b25eb3034b80f0de4eab17
kakuilan <kakuilan@163.com>
2020-09-14 09:39:44 +0800

    add 路由测试

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('foo', function () {
+    return 'Hello World';
+});
+Route::match(['get', 'post', 'put'], 'time', function () {
+    return date('Y-m-d H:i:s');
+});
+Route::get('/user', 'UserController@index');
